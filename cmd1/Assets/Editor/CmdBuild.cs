cc756ab7679300817ba948a4cf97795af23d8a77
MrWzl <734987891@qq.com>
2022-06-10 18:34:32 +0800

    1
    1

--- cmd1/Assets/Editor/CmdBuild.cs
+++ cmd1/Assets/Editor/CmdBuild.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,7 +9,22 @@ public class CmdBuild
 {
     [MenuItem("Cmd/Build",false,1)]
     public static void PerformBuild() {
-        string[] scenes = { "Assets/Scenes/SampleScene.unity" };
-        BuildPipeline.BuildPlayer(scenes, "cmd.apk",BuildTarget.Android,BuildOptions.None);
+        var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+        var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+        //文件夹处理
+        if(!Directory.Exists(outdir))
+            Directory.CreateDirectory(outdir);
+        if(File.Exists(outputPath))
+            File.Delete(outputPath);
+
+        //开始项目一键打包
+        string[] scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        if(File.Exists(outputPath)) {
+            Debug.Log("Build Success :" + outputPath);
+        } else {
+            Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        }
+
     }
 }
